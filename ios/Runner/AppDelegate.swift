6fb70572230811a9bc0588ae28059b74b49c8d65
ec2-user <ec2-user@ip-172-31-12-231.eu-west-1.compute.internal>
2025-04-09 21:00:48 +0000

    iOS setup progress

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,18 @@
 import UIKit
 import Flutter
+// import awesome_notifications
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Initialize awesome_notifications
+    // SwiftAwesomeNotificationsPlugin.setPluginRegistrantCallback { registry in
+    //   GeneratedPluginRegistrant.register(with: registry)
+    // }
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
