c78ed18b84fe64a6ac8bb422ef6f8ef60cd6eed7
Tafseer App <tafseerapp@IKYSM1MBP.local>
2025-07-29 18:45:50 +0300

    iOS: Fix bridging header and plugin registration issues

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,10 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    // Register plugins manually without bridging header
+    if let registrar = self.registrar(forPlugin: "GeneratedPluginRegistrant") {
+      // This will be handled by Flutter's automatic plugin registration
+    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
